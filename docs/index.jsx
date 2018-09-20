d17db62d9bd2a655743cb8ca8ce53a87c10d2fb9
RubyLouvre <1669866773@qq.com>
2018-09-20 14:53:55 +0800

    更新

--- docs/index.jsx
+++ docs/index.jsx
@@ -4,10 +4,11 @@ banner:
   desc: '基于 React 编写小程序的转译框架'
   btns: 
     - { name: '开 始', href: './documents/intro.html', primary: true }
-    - { name: 'Github >', href: 'https://github.com/roland-reed/nanachi-cli' }
+    - { name: 'Github >', href: 'https://github.com/RubyLouvre/nanachi' }
   caption: '当前版本: v0.0.2'
 features: 
   - { name: '三套模板', desc: '旅游、商城、企业三套通用模板' }
+  - { name: '多端支持', desc: '支持微信、百度、支付宝小程序，快应用支持跟进中' }
   - { name: '支持 slot', desc: '突破小程序 template 限制，支持组件标签包含其他内容' }
   - { name: '支持事件传参', desc: '突破 wxml 的限制，实现多次 bind this 与参数' }
   - { name: '组件化编程', desc: '完美兼容 React 生命周期钩子' }
