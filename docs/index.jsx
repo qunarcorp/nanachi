8ec7f24a29c376f87a8149231f2b83fae9315b00
RubyLouvre <1669866773@qq.com>
2019-01-07 11:08:59 +0800

    update首页

--- docs/index.jsx
+++ docs/index.jsx
@@ -8,7 +8,7 @@ banner:
   caption: ''
 features: 
   - { name: '三套模板', desc: '旅游、商城、音乐三套通用模板' }
-  - { name: '多端支持', desc: '支持微信、百度、支付宝小程序与快应用的转译，头条小程序正在跟进中' }
+  - { name: '多端支持', desc: '支持微信、百度、支付宝、头条小程序与快应用的转译，H5 SPA正在跟进中' }
   - { name: '支持 slot', desc: '突破小程序 template 限制，支持组件标签包含其他内容' }
   - { name: '支持事件传参', desc: '突破 wxml 的限制，实现多次 bind this 与参数' }
   - { name: '组件化编程', desc: '完美兼容 React 生命周期钩子' }
@@ -23,8 +23,8 @@ footer:
       - { name: 'YMFE', href: 'https://ymfe.org/' }
       - { name: 'YMFE Blog', href: 'https://blog.ymfe.org/' }
     Git仓库:
-      - { name: 'Github', href: 'https://github.com/YMFE/ydoc' }
-      - { name: 'Github Issue', href: 'https://github.com/YMFE/ydoc/issues' }
+      - { name: 'Github', href: 'https://github.com/RubyLouvre/anu' }
+      - { name: 'Github Issue', href: 'https://github.com/RubyLouvre/anu/issues' }
 
 ---
 
